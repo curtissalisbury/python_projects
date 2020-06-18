8bdecdfde13e53bab2896562d6507f547cd41277
Curtis Salisbury <curtis.salisbury@gmail.com>
2020-06-18 14:43:30 -0600

    got tests working with negative and 0 numbers

--- web_services/conversions/formula_tests.py
+++ web_services/conversions/formula_tests.py
@@ -11,13 +11,18 @@ class FormulaTests(unittest.TestCase):
             {'value': 3.5, 'expected_value': 2.17},
             {'value': 2375, 'expected_value': 1475.75},
             {'value': 22.698, 'expected_value': 14.10},
-            # {'value': 0.0, 'expected_value': 0.0}
+            {'value': 0.0, 'expected_value': ValueError},
+            {'value': -10.5, 'expected_value': ValueError}
         ]
 
         for item in kilometer_test_values:
-            self.assertEqual(
-                formulas.kilometers_to_miles(kilometers=item.get('value')),
-                item.get('expected_value'))
+            try:
+                self.assertEqual(
+                    formulas.kilometers_to_miles(kilometers=item.get('value')),
+                    item.get('expected_value'))
+            except ValueError as e:
+                if isinstance(e, item.get('expected_value')):
+                    pass
 
     def test_miles_to_kilometers(self):
         miles_test_values = [
@@ -25,10 +30,16 @@ class FormulaTests(unittest.TestCase):
             {'value': 3.0, 'expected_value': 4.83},
             {'value': 3.5, 'expected_value': 5.63},
             {'value': 2375, 'expected_value': 3822.19},
-            {'value': 22.698, 'expected_value': 36.53}
+            {'value': 22.698, 'expected_value': 36.53},
+            {'value': 0.0, 'expected_value': ValueError},
+            {'value': -10.5, 'expected_value': ValueError}
         ]
 
         for item in miles_test_values:
-            self.assertEqual(
-                formulas.miles_to_kilometers(miles=item.get('value')),
-                item.get('expected_value'))
+            try:
+                self.assertEqual(
+                    formulas.miles_to_kilometers(miles=item.get('value')),
+                    item.get('expected_value'))
+            except ValueError as e:
+                if isinstance(e, item.get('expected_value')):
+                    pass
