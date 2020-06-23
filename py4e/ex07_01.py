6d247f56665fbf9bd5e9493eceecc6cb452d7be1
Curtis Salisbury <curtis.salisbury@gmail.com>
2020-06-23 14:24:25 -0600

    py4e exercise 7.1 better error handling

--- py4e/ex07_01.py
+++ py4e/ex07_01.py
@@ -2,8 +2,8 @@ fname = input('Enter File Name: ')
 
 try:
     fhand = open(fname)
-except:
-    print('Enter a valid filename', fhand)
+except FileNotFoundError:
+    print(f'file "{fname}" cannot be found in working directory')
     quit()
 
 for line in fhand:
